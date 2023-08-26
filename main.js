6c0d4fa71824193a12e688b2fce6ab7df4d011f0
DineshKumarPatro98 <dineshkumarpatro9@gmail.com>
2023-08-26 09:33:53 +0530

    add file

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Heloooo")
+
+function increaseValue(number) {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue(number) {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
